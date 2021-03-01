88968c109569f242fa658e487117e4691dc1df13
zmqingfeng <zmqingfeng@163.com>
2021-03-02 01:25:04 +0800

    项目切换至springboot项目

---
+++ src/main/java/com/paul/LearningProgressApplication.java
@@ -0,0 +1,11 @@
+package com.paul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProgressApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProgressApplication.class, args);
+	}
+}
